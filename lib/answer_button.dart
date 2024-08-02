fbfc5f53334660a391ff78ecb27541700cbff9a7
Andrew Brower <abrower10612@gmail.com>
2024-08-02 13:55:01 -0600

    fix width of AnswerButton widget

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -12,21 +12,24 @@ class AnswerButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      style: ElevatedButton.styleFrom(
-        padding: const EdgeInsets.symmetric(
-          vertical: 10,
-          horizontal: 40,
+    return SizedBox(
+      width: 200,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 40,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(40),
+          ),
         ),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(40),
-        ),
-      ),
-      child: Text(
-        answerText,
-        style: const TextStyle(
-          color: Colors.black,
+        child: Text(
+          answerText,
+          style: const TextStyle(
+            color: Colors.black,
+          ),
         ),
       ),
     );
